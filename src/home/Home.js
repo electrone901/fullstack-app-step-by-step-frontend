e71996bf093ba5571f6def70bda3b35d156a50a5
electrone901 <carbajalluis@rocketmail.com>
2018-10-08 16:57:14 -0400

    add react-router to connect all componets

--- src/home/Home.js
+++ src/home/Home.js
@@ -7,23 +7,33 @@ import { Link } from 'react-router-dom';
 
 
 class Home extends Component {
+	constructor(){
+		super();
+		this.state = {
+			isAunthenticated: false
+		}
+	}
 	render() {
 		// const {isAunthenticated} = this.props.auth;  
+		// const {isAunthenticated} = false;  
 		const authLink = (
-			<div>
-				<li className="btn bnt-light">
-					<a className="nav-link"> Porfolio</a>
-				</li>
-			</div>
+			<button type="button">
+				<Link to="/portfolio" className="btn btn-primary">Porfolio</Link>
+			</button>
+		);
+		const register = (
+			<button type="button">
+				<Link to="/register" className="btn btn-primary">Sign Up</Link>
+			</button>
 		)
+
 		return (
 			<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
 			  <div className="carousel-inner">
 			    <div className="carousel-item active">
 			    <div className="carousel-caption d-none d-md-block">
 				    <h3 className="addColor">Invest in companies with high growth potential that are improving the world. Invest as Low as $50. Choose Your Portfolio Mix.</h3>
-				    <button className="btn btn-primary" type="button">Start Now</button>
-				    <button className="btn btn-success" type="button">Portfolio</button>
+				    {this.state.isAunthenticated ? authLink: register}
 				  </div>
 			      <img className="d-block w-100" src={Img1} alt="First slide"/>
 			    </div>
